eada3e5fcdea8bf4e98f191ca86369e6cd82c273
Chris LaRose <cjlarose@gmail.com>
2015-04-06 00:57:18 -0700

    Add license and setup files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name = "pyeventstore",
+    packages = ["pyeventstore"],
+    version = "0.0.1",
+    description = "Client library for Event Store",
+    author = "Chris LaRose",
+    author_email = "cjlarose@gmail.com",
+    url = "https://github.com/cjlarose/pyeventstore"
+)
