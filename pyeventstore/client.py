a7d0fd210b44f5284fd217980c123312158446b6
Chris LaRose <cjlarose@gmail.com>
2015-04-11 13:20:18 -0700

    Rename methods

--- pyeventstore/client.py
+++ pyeventstore/client.py
@@ -1,10 +1,9 @@
 import asyncio
-import uuid
-import json
 
 from pyeventstore.events import (get_all_events,
                                  start_subscription,
                                  publish_events)
+
 
 class Client:
 
@@ -14,18 +13,18 @@ class Client:
 
     @asyncio.coroutine
     def publish_events(self, stream_name, events):
-        uri = self.stream_head_uri(stream_name)
+        uri = self._stream_head_uri(stream_name)
         yield from publish_events(uri, events)
 
-    def stream_head_uri(self, stream_name):
+    def _stream_head_uri(self, stream_name):
         return '{}/streams/{}'.format(self.uri_base, stream_name)
 
     @asyncio.coroutine
     def get_all_events(self, stream_name):
-        head_uri = self.stream_head_uri(stream_name)
+        head_uri = self._stream_head_uri(stream_name)
         return (yield from get_all_events(head_uri))
 
     @asyncio.coroutine
     def subscribe(self, stream_name, interval_seconds=1):
-        head_uri = self.stream_head_uri(stream_name)
+        head_uri = self._stream_head_uri(stream_name)
         return (yield from start_subscription(head_uri, interval_seconds))
